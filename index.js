42ec92cead35abfb9ac25bf773020c7505f7386d
Giovanna <nana.sousa453@gmail.com>
2024-05-17 13:31:10 -0300

    Desenvolvendo lógica de inserção de dados no MySQL e salvando imagens na pasta 'uploads'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const mysql = require('mysql2');
+const mysql2 = require('mysql2');
 const multer = require('multer');
 const sharp = require('sharp');
 const cors = require('cors');
@@ -18,32 +18,47 @@ app.use(
 );
 
 const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.render('index', {css: '/style/home.css', title: ''})
+    res.render('index', { css: '/style/home.css', title: '' });
 });
 
 app.get('/create', (req, res) => {
-    res.render('newCorreio', {css: '', title: ''})
+    res.render('newCorreio', { css: '', title: '' });
 });
 
 app.post('/upload', upload.single('image'), async (req, res) => {
-    try{
+    try {
 
-        const imgName = req.file.originalname;
         const imgData = req.file.buffer;
+        const imgName = req.file.originalname;
+        const title = req.body.title;
+        const category = req.body.category;
+        const post = req.body.post;
 
         await sharp(imgData).toFile(`uploads/${imgName}`);
 
-        res.send('Imagem salva com sucesso!')
+        const sql = `INSERT INTO Correios (title, category, post, imageName)
+        VALUES ('${title}', '${category}', '${post}', '${imgName}')`;
 
-    } catch (error) {
-        res.status(500).send('Erro ao salvar imagem!')
+        conn.query(sql, function (err) {
+            if (err) {
+                console.log('erro: ', err);
+                return false;
+            }
+    
+            res.redirect('/');
+        })
+
+    } catch (err) {
+        console.error('Erro:', err);
+        res.status(500).send('Erro ao adicionar dados!');
     }
+
 });
 
 /* app.get('/login', (req, res) => {
@@ -54,4 +69,21 @@ app.get('/signUp', (req, res) => {
     res.render('signUp', {css: '/style/login.css', title: '| Cadastre-se'})
 }); */
 
-app.listen(3001);
+const conn = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'flight'
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('Conectado ao MySQL');
+    app.listen(3001, () => {
+        console.log('Servidor rodando na porta 3001');
+    });
+});
