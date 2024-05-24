562f3ef4a192b4f23edcd3e0d53aef2e1b95a75b
Giovanna <nana.sousa453@gmail.com>
2024-05-24 15:50:34 -0300

    Exibindo posts de acordo com data de publicação, resolvendo bugs e aprimorando css

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const sharp = require('sharp');
 const cors = require('cors');
 const session = require('express-session');
 const fs = require("fs");
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static('assets'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors());
 
 app.use(
@@ -32,11 +34,11 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.render('login', {css: '/style/login.css', title: '| Login'})
+    res.render('login', { css: '/style/login.css', title: '| Login' })
 });
 
 app.post('/login', (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
     const sql = `SELECT * FROM Users WHERE username = '${username}'`;
 
     conn.query(sql, [username], (err, results) => {
@@ -45,31 +47,31 @@ app.post('/login', (req, res) => {
             return false;
         }
 
-        if(results.length === 0) {
-            return res.status(400).render('login', {css: '/style/login.css', message: 'Usuário não encontrado!'})
+        if (results.length === 0) {
+            return res.status(400).render('login', { css: '/style/login.css', message: 'Usuário não encontrado!' })
         }
 
         const user = results[0];
 
         if (user.password_ !== password) {
 
-            return res.status(400).render('login', {css: '/style/login.css', message: 'Senha Incorreta!'})
+            return res.status(400).render('login', { css: '/style/login.css', message: 'Senha Incorreta!' })
         }
 
         console.log('Usuário autenticado:', user)
 
-        req.session.user = {id: user.id, username: user.username};
+        req.session.user = { id: user.id, username: user.username };
         console.log('Sessão do usuário:', req.session.user)
         res.redirect('/home');
-    })   
+    })
 });
 
 app.get('/signUp', (req, res) => {
-    res.render('signUp', {css: '/style/login.css', title: '| Cadastre-se'})
+    res.render('signUp', { css: '/style/login.css', title: '| Cadastre-se' })
 });
 
 app.post('/upSign', (req, res) => {
-    const {username, name, email, password} = req.body;
+    const { username, name, email, password } = req.body;
 
     const checkUsername = `SELECT * FROM Users WHERE username = '${username}'`;
     conn.query(checkUsername, [username], (err, data) => {
@@ -78,7 +80,7 @@ app.post('/upSign', (req, res) => {
             return false;
         }
 
-        if(data.length > 0) {
+        if (data.length > 0) {
             return res.status(400).send('Username já existe! Faça login ou escolha outra.')
         }
 
@@ -90,24 +92,44 @@ app.post('/upSign', (req, res) => {
                 console.log('erro: ', err);
                 return false;
             }
-    
+
             res.redirect('/home');
         })
     })
 });
 
 app.get('/home', (req, res) => {
-    
+
     const sql = `SELECT Correios.*, Users.username, Users.name_ 
-    FROM Correios JOIN Users ON Correios.user_id = Users.id`;
+    FROM Correios JOIN Users ON Correios.user_id = Users.id
+    ORDER BY Correios.created_at DESC`;
 
     conn.query(sql, function (err, data) {
-        if(err){
+        if (err) {
             console.log('erro: ', err)
             return
         }
 
-        res.render('index', { css: '/style/home.css', title: '', user: req.session.user, correios: data});
+        res.render('index', { css: '/style/home.css', title: '', user: req.session.user, correios: data });
+    })
+});
+
+app.get('/home/category/:category', (req, res) => {
+    const category = req.params.category;
+
+    console.log(category)
+
+    const sql = `SELECT Correios.*, Users.username, Users.name_ 
+    FROM Correios JOIN Users ON Correios.user_id = Users.id 
+    WHERE category = '${category}'`;
+
+    conn.query(sql, function (err, data) {
+        if (err) {
+            console.log('erro: ', err)
+            return
+        }
+
+        res.render('index', { css: '/style/home.css', title: '', user: req.session.user, correios: data, category});
     })
 });
 
@@ -116,7 +138,7 @@ app.get('/create', (req, res) => {
     function checkAuth(req, res, next) {
         if (!req.session.user) {
             req.session.message = 'Você precisa estar logado para criar um post!';
-            return res.redirect('/showMessage'); 
+            return res.redirect('/showMessage');
         }
         next();
     }
@@ -137,13 +159,16 @@ app.post('/upload', upload.single('image'), async (req, res) => {
         }
 
         const id = req.session.user.id;
-        const imgData = req.file.buffer;
-        const imgName = req.file.originalname;
         const title = req.body.title;
         const category = req.body.category;
         const post = req.body.post;
 
-        await sharp(imgData).toFile(`uploads/${imgName}`);
+        let imgName = null;
+        if (req.file) {
+            const imgData = req.file.buffer;
+            imgName = req.file.originalname;
+            await sharp(imgData).toFile(`uploads/${imgName}`);
+        }
 
         const sql = `INSERT INTO Correios (title, category, post, imageName, user_id)
         VALUES ('${title}', '${category}', '${post}', '${imgName}', '${id}')`;
@@ -153,7 +178,7 @@ app.post('/upload', upload.single('image'), async (req, res) => {
                 console.log('erro: ', err);
                 return false;
             }
-    
+
             res.redirect('/home');
         })
 
@@ -161,27 +186,6 @@ app.post('/upload', upload.single('image'), async (req, res) => {
         console.error('Erro:', err);
         res.status(500).send('Erro ao adicionar dados!');
     }
-});
-
-app.get('/upload/imagens', (req, res) => {
-    fs.readdir("uploads/", (err, files) => {
-        if (err) {
-          return res.status(500).send("Erro ao listar imagens.");
-        }
-    
-        const images = files.filter(
-          (file) => file.endsWith(".jpg") || file.endsWith(".png") || file.endsWith(".jpeg")
-        );
-        res.send(images);
-      });
-});
-
-const path = require('path');
-
-app.get('/upload/imagens/:imageName', (req, res) => {
-    const imageName = req.params.imageName;
-    const imagePath = path.join(__dirname, 'uploads', imageName);
-    res.sendFile(imagePath);
 });
 
 app.get('/logout', (req, res) => {
